6e0bd66107c909206e5d4a742838d9752c6e9def
Nirmalya247 <nrmlgn247@gmail.com>
2021-07-29 22:37:18 +0530

    d1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,18 +1,19 @@
 import { Component } from '@angular/core';
 @Component({
-  selector: 'app-root',
-  templateUrl: 'app.component.html',
-  styleUrls: ['app.component.scss'],
+    selector: 'app-root',
+    templateUrl: 'app.component.html',
+    styleUrls: ['app.component.scss'],
 })
 export class AppComponent {
-  public appPages = [
-    { title: 'Inbox', url: '/folder/Inbox', icon: 'mail' },
-    { title: 'Outbox', url: '/folder/Outbox', icon: 'paper-plane' },
-    { title: 'Favorites', url: '/folder/Favorites', icon: 'heart' },
-    { title: 'Archived', url: '/folder/Archived', icon: 'archive' },
-    { title: 'Trash', url: '/folder/Trash', icon: 'trash' },
-    { title: 'Spam', url: '/folder/Spam', icon: 'warning' },
-  ];
-  public labels = ['Family', 'Friends', 'Notes', 'Work', 'Travel', 'Reminders'];
-  constructor() {}
+    public defaultPages = [
+        { title: 'Home', url: '/home', icon: 'home', id: 'home' },
+    ];
+    public products = [
+        { title: 'Electronics', icon: '', id: 'electronics', sub: [
+            { title: 'Mobile', icon: '', url: '/mobile' }
+        ] }
+    ];
+    isRemainder = 0;
+    selectedId = 'home';
+    constructor() { }
 }
